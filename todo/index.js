e668e17783f9b30acfd9ba4bdefe0982e1364161
rahulxdev0 <rk854327@gmail.com>
2025-11-30 23:12:15 +0530

    new changes

--- todo/index.js
+++ todo/index.js
@@ -1,18 +1,20 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 8000;
+import cors from "cors";
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 let todos = [];
 
 app.get("/todos", (req, res) => {
-  res.json(todos);
+  res.status(200).json(todos);
 });
 
 app.post("/posts", (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
   const { title } = req.body;
   if (!title) return res.status(400).json({ message: "Title is required" });
 
@@ -24,6 +26,21 @@ app.post("/posts", (req, res) => {
 
   todos.push(newTodo);
   res.status(201).json(newTodo);
+});
+
+app.delete("/todo/:id", (req, res) => {
+  const { id } = req.params;
+  const todoId = Number(id); // convert string param to number
+
+  const index = todos.findIndex((item) => item.id === todoId);
+  if (index === -1) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  // remove todo from array
+  todos.splice(index, 1);
+
+  return res.status(200).json({ message: "Todo deleted successfully", todos });
 });
 
 app.listen(PORT, () => {
